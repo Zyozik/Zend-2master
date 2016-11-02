88a2396bea5821b40d85018a224bc333c0849856
Zyozik <zyozya_15@mail.ru>
2016-11-02 14:14:07 +0200

    Initial commit

---
+++ config/application.config.php
@@ -0,0 +1,25 @@
+<?php
+return array(
+    'modules' => array(
+        'ZfcBase',
+        'ZfcUser',
+        'BjyAuthorize',
+        'RoleUserBridge',
+    	'BjyProfiler',
+    	'ZFTool',
+        'Application',
+        'Admin',
+    	'Album',
+        'Form',
+        
+    ),
+    'module_listener_options' => array(
+        'config_glob_paths'    => array(
+            'config/autoload/{,*.}{global,local}.php',
+        ),
+        'module_paths' => array(
+            './module',
+            './vendor',
+        ),
+    ),
+);
